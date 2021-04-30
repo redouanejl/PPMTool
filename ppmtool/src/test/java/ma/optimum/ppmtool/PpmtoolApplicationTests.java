634ed219c278f32380f5b064628b370c04747696
redouanejl <35847270+redouanejl@users.noreply.github.com>
2021-04-30 11:46:32 +0000

    first commit

---
+++ ppmtool/src/test/java/ma/optimum/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.optimum.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
